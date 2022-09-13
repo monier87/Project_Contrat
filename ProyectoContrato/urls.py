da69e326d85c9387bd4a18b20a9b4cdd1d294c04
Cinolokis Cobas <cinolkis87@gmail.com>
2022-09-13 11:12:53 -0300

    ok

--- ProyectoContrato/urls.py
+++ ProyectoContrato/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('Contratos/', include('Contratos.urls')),
     path('', include('proyectoWebapp.urls')),
+    path('Contratos/', include('Contratos.urls')),
 ]
 
